a412f94ea0e6ed952568230b867896c7ac85488f
NM <masihnishit@gmail.com>
2022-10-01 21:54:00 +0530

    video #3

---
+++ src/main/java/com/alpha/www/MapStructDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapStructDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapStructDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpha/www/MapStructDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapStructDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
